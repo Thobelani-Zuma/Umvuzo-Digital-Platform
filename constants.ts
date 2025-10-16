085f0c7fa53e1ae18460711bfed4a45c793296ac
Thobelani-Zuma <richhomietzee9@gmail.com>
2025-10-16 11:26:26 +0200

    feat: Update material rates
    Adjusted prices for various recyclable materials in the RATE_SHEETS constant to reflect current market values.

--- constants.ts
+++ constants.ts
@@ -2,15 +2,15 @@ import { RateSheet, Transaction } from './types';
 
 export const RATE_SHEETS: RateSheet = {
   "Walk-ins": [
-    { type: "PET Clear", price: 3.1 },
-    { type: "PET Green", price: 2.0 },
+    { type: "PET Clear", price: 3.60 },
+    { type: "PET Green", price: 3.00 },
     { type: "PET Brown", price: 1.8 },
     { type: "C-oil", price: 1.3 },
-    { type: "HDPE", price: 1.5 },
+    { type: "HDPE", price: 2.00 },
     { type: "Tins", price: 1.0 },
-    { type: "Cans", price: 15.5 },
-    { type: "C-plastic", price: 1.7 },
-    { type: "M-plastic", price: 1.5 },
+    { type: "Cans", price: 16.50 },
+    { type: "C-plastic", price: 2.00 },
+    { type: "M-plastic", price: 1.00 },
     { type: "W-paper", price: 1.0 },
     { type: "K4", price: 0.8 },
     { type: "TetraPak", price: 0.5 },
