8e24d67f0bd8a1619c8dc836bf03c312f5fe1704
Thobelani-Zuma <richhomietzee9@gmail.com>
2025-11-26 11:50:14 +0200

    fix: Update PET Green and K4 prices
    Adjusted the pricing for "PET Green" and "K4" materials in the constants file to reflect current rates. Removed unused GoogleGenAI import from TransactionsPage.

--- pages/TransactionsPage.tsx
+++ pages/TransactionsPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useMemo, useRef } from 'react';
-import { GoogleGenAI } from '@google/genai';
 import { Transaction } from '../types';
 import { RATE_SHEETS } from '../constants';
 import { PlusIcon, TrashIcon, PrintIcon, ScaleIcon, CameraIcon } from '../components/icons/Icons';
